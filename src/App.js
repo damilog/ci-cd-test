77b518cd1e7465afa77b7332b1307b41c1ea81d7
damilog <dami.k.dev@gmail.com>
2023-06-06 11:50:46 +0900

    feat: Remove react link and Change title

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <h1>
          내가 바로 P다!
         </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
