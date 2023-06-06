7ebe23937f13bd50eb09c5918817919e51ed1f44
damilog <dami.k.dev@gmail.com>
2023-06-06 10:33:26 +0900

    chore: install react

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './daisy.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+         내가 1등 P다!
+        </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
